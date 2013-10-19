16565ecff1013fd3ba3fc61cb695397fe31ea1dc
Kathryn Killebrew <kathryn.killebrew@gmail.com>
2013-10-19 12:34:51 -0400

    Ignore env directory

--- env/lib/python2.7/site-packages/django/db/migrations/operations/__init__.py
+++
@@ -1,3 +0,0 @@
-from .models import CreateModel, DeleteModel, AlterModelTable, AlterUniqueTogether, AlterIndexTogether
-from .fields import AddField, RemoveField, AlterField, RenameField
-from .special import SeparateDatabaseAndState, RunSQL, RunPython
